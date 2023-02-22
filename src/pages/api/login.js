c21bf785e40ab8779ff0b7edecd40d4c5b48af35
Randy Sim <randysim9@gmail.com>
2023-02-22 15:55:36 -0500

    login backend for now

---
+++ src/pages/api/login.js
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method != "POST") return res.status(400).json({ success: false, error: "Expected post request." });
+
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (!email) return res.status(400).json({ success: false, error: "Missing Email." });
+    if (!password) return res.status(400).json({ success: false, error: "Missing Password." });
+
+    let user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+
+    if (user == null) return res.status(200).json({ success: false, error: "Invalid Email." });
+
+    bcrypt.compare(password, user.password, (err, match) => {
+        if (err) return res.status(500);
+
+        if (match) {
+            return res.status(200).json({ success: true, authToken: user.authToken });
+        } else {
+            return res.status(200).json({ success: false, error: "Passwords do not match."});
+        }
+    });
+}
