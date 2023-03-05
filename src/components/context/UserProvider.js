a7a98914cc15fea2e4bddd4e99f93e100bb0733b
Randy Sim <randysim9@gmail.com>
2023-03-05 14:17:33 -0500

    fixes

--- src/components/context/UserProvider.js
+++ src/components/context/UserProvider.js
@@ -22,7 +22,6 @@ const UserProvider = ({ children }) => {
 
     // Attempt Sign In if not signed in
     if (!userState.signedIn) {
-<<<<<<< HEAD
       axios.get("api/login")
         .then(res => {
           let data = res.data;
@@ -38,15 +37,6 @@ const UserProvider = ({ children }) => {
             router.push("/");
           }
         })
-=======
-      axios.get("api/login").then((res) => {
-        let data = res.data;
-        if (data.success) {
-          console.log(`Cookie Sign In`);
-          setUserState({ signedIn: true });
-        }
-      });
->>>>>>> 1b4f75f215b2ae9ee8f717b0781b5773fc8e10f0
     }
   });
 
