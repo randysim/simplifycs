e6e3783cda632e11603453f40eeec11b132207fe
Randy Sim <randysim9@gmail.com>
2023-03-12 16:04:07 -0400

    theme provider template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 
 import UserProvider from "@/components/context/UserProvider";
+import ThemeProvider from "@/components/context/ThemeProvider";
 
 export default function App({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ThemeProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
