3876ee05ecc6622cf56da1c6ed3168cd5cf6e0b2
Randy Sim <randysim9@gmail.com>
2023-03-12 16:04:07 -0400

    theme provider template

--- src/components/dashboard/CourseCard.js
+++ src/components/dashboard/CourseCard.js
@@ -1,24 +1,29 @@
 import { Card, Typography, Divider, Grid } from "@mui/material";
 
 function CourseCard({ id, title, description }) {
-    return (
-        <Grid item>
-            <Card 
-                sx={{ cursor: "pointer", maxWidth: 600, minWidth: 400, minHeight: 200, maxHeight: 300, padding: "10px" }}
-            >
-                <Typography
-                    variant={"h5"}
-                    sx={{ fontWeight: "bold", fontSize: "1.3rem" }}
-                >
-                    {title}
-                </Typography>
-                <Divider sx={{ my: 1 }} />
-                <>
-                    {description}
-                </>
-            </Card>
-        </Grid>
-    )
+  return (
+    <Grid item>
+      <Card
+        sx={{
+          cursor: "pointer",
+          maxWidth: 600,
+          minWidth: 400,
+          minHeight: 200,
+          maxHeight: 300,
+          padding: "10px",
+        }}
+      >
+        <Typography
+          variant={"h5"}
+          sx={{ fontWeight: "bold", fontSize: "1.3rem" }}
+        >
+          {title}
+        </Typography>
+        <Divider sx={{ my: 1 }} />
+        <>{description}</>
+      </Card>
+    </Grid>
+  );
 }
 
 export default CourseCard;
